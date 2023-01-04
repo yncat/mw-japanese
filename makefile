0db76799907208a5c62ca65c4d6344a573d78b62
Yukio Nozawa <personal@nyanchangames.com>
2023-01-04 17:24:36 +0900

    Translate terrain

--- makefile
+++ makefile
@@ -1,3 +1,8 @@
-.PHONY: init
+.PHONY: init translate
+
+translate:
+	python scripts/translate_terrain.py japanese/terrain.txt
+
 init:
 	pip install -r requirements.txt
+
--- scripts/translator.py
+++ scripts/translator.py
@@ -8,10 +8,12 @@ translated_cache = {}
 
 translator = deepl.Translator(API_KEY)
 
-def translate(text):
-	if text in translated_cache:
-		return translated_cache[text]
-	output = translator.translate_text(text, target_lang = "JA")
-	translated_cache[text] = output.text
-	return output.text
 
+def translate(text):
+    if text in translated_cache:
+        print("%s => %s (cached)" % (text, translated_cache[text]))
+        return translated_cache[text]
+    output = translator.translate_text(text, target_lang="JA")
+    translated_cache[text] = output.text
+    print("%s => %s" % (text, output.text))
+    return output.text
